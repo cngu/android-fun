4c36b959f1a68919e11360360f2348a9b5e50d15
cngu <chris.s.nguyen@gmail.com>
2015-06-06 11:57:51 -0600

    Moved toolbar menu creation and handling from MainActivity into MainFragment to fully separate them now.

--- app/src/main/java/com/cngu/androidfun/MainActivity.java
+++ app/src/main/java/com/cngu/androidfun/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,33 +37,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // TODO: Init MainFragment
+
         // We only need to attach the fragment when this Activity is first opened.
         if (savedInstanceState == null) {
             fragmentManager.beginTransaction()
                     .add(R.id.content_fragment_container, mView.asFragment(), FRAGMENT_TAG_MAIN)
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/cngu/androidfun/MainFragment.java
+++ app/src/main/java/com/cngu/androidfun/MainFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -35,6 +39,12 @@ public class MainFragment extends BaseFragment implements IMainFragment {
     public MainFragment() {}
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -45,5 +55,28 @@ public class MainFragment extends BaseFragment implements IMainFragment {
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_main, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
