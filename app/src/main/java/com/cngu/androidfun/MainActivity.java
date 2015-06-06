2592fc31e1d6b4d733163b1179ab48fa5394e67f
cngu <chris.s.nguyen@gmail.com>
2015-06-05 22:40:59 -0600

    Completely moved activity_main views into fragment_main to keep activities as dumb as possible. Removed drop shadows under Toolbar because we'll be placing a TabLayout underneath of it later for a ViewPager.

--- app/src/main/java/com/cngu/androidfun/MainActivity.java
+++ app/src/main/java/com/cngu/androidfun/MainActivity.java
@@ -3,7 +3,6 @@ package com.cngu.androidfun;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Use the toolbar as our Action Bar (i.e. not in standalone mode)
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         //
         // Initialize helper *Managers
--- app/src/main/java/com/cngu/androidfun/MainFragment.java
+++ app/src/main/java/com/cngu/androidfun/MainFragment.java
@@ -1,11 +1,15 @@
 package com.cngu.androidfun;
 
 
-import android.os.Bundle;;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+;
 
 
 /**
@@ -34,8 +38,12 @@ public class MainFragment extends BaseFragment implements IMainFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Use the toolbar as our Action Bar (i.e. not in standalone mode)
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        return view;
     }
-
-
 }
